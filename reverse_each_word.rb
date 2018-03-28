3d8c3c3954bc30cb33d2673887c122ee82b74ec3
Joseph Parker <jparker010190@gmail.com>
2018-03-28 02:22:27 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,15 +1,13 @@
 def reverse_each_word(string)
   array = string.split
   array.each do |reve|
-    num = reve.reverse
-    puts"#{num}"
+    reve.reverse
   end
 end
 
 def reverse_each_word(string)
   array = string.split
   array.collect do |reve|
-    num = reve.reverse
-    puts"#{num}"
+    reve.reverse
   end
 end
